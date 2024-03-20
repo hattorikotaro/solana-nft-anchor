596685608ed63b62f221c7383537e36768c59021
hattorikotaro <164205419+hattorikotaro@users.noreply.github.com>
2024-03-20 22:19:01 +0700

    Update lib.rs

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("3HLQXEf9FSmtoGK6JrmxM7pMzuooEhg1rRLtj4x36T3A");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
